252599aaee95e78631a8607a0029fa186779ca28
mozahidsani <mozahidwebmin11d10@gmail.com>
2023-08-27 18:58:56 +0600

    some

---
+++ javaScript/Hoisting.js
@@ -0,0 +1,19 @@
+//01. 
+x = 5 //assignment
+console.log(x)
+var x //declare 
+
+//convert for var this is a main concept demo
+var x
+x =undefined
+console.log(x)
+
+//var can not set x value with undefined
+
+
+
+
+
+
+
+
